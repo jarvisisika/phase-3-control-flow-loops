069aac64dd3c45556c2d921f66264daeb459f1a0
jarvisisika <jarvis.isika@student.moringaschool.com>
2023-05-18 04:53:15 +0300

    completed assignment

--- looping.rb
+++ looping.rb
@@ -1,24 +1,38 @@
+# Solution for the 'happy_new_year' method
 def happy_new_year
-  # your code here
+  count = 10
+  while count > 0
+    puts count
+    count -= 1
+  end
+  puts "Happy New Year!"
 end
 
-# No need to modify this code! Use this to implement the fizzbuzz_printer method.
-def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
-  elsif num % 3 == 0
-    "Fizz"
-  elsif num % 5 == 0
-    "Buzz"
-  else
-    num
+# Solution for the 'reverse_string' method
+def reverse_string(string)
+  reversed = ""
+  index = string.length - 1
+  while index >= 0
+    reversed += string[index]
+    index -= 1
+  end
+  reversed
+end
+
+# Solution for the 'fizzbuzz' method
+def fizzbuzz_printer
+  (1..100).each do |number|
+    if number % 3 == 0 && number % 5 == 0
+      puts "FizzBuzz"
+    elsif number % 3 == 0
+      puts "Fizz"
+    elsif number % 5 == 0
+      puts "Buzz"
+    else
+      puts number
+    end
   end
 end
 
-def fizzbuzz_printer
-  # your code here
-end
-
-def reverse_string(str)
-  # your code here
-end
+# Call the method to print the fizzbuzz sequence
+fizzbuzz_printer
